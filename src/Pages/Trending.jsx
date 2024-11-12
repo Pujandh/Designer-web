4b71dc83fc026120df00aadf609681e93ca20486
Pujandh <pujandhungana103@gmail.com>
2024-11-12 09:23:32 +0545

    Update Trending.jsx

--- src/Pages/Trending.jsx
+++ src/Pages/Trending.jsx
@@ -12,14 +12,14 @@ const Trending = () => {
 
       {/* Grid layout for displaying images */}
       <div className='gridContainer'>
-        <img src='/images/bedroom.jpeg' className='grid-image' alt='Bedroom' onError={(e) => e.target.src = '/images/default.jpg'} />
-        <img src='/images/chair.jpg' className='grid-image' alt='Chair' onError={(e) => e.target.src = '/images/default.jpg'} />
-        <img src='/images/circlemirror.jpg' className='grid-image' alt='Circle Mirror' onError={(e) => e.target.src = '/images/default.jpg'} />
-        <img src='/images/dinnertable.jpg' className='grid-image' alt='Dinner Table' onError={(e) => e.target.src = '/images/default.jpg'} />
-        <img src='/images/exteriordesign.jpeg' className='grid-image' alt='Exterior Design' onError={(e) => e.target.src = '/images/default.jpg'} />
-        <img src='/images/interiorbed.jpg' className='grid-image' alt='Interior Bed' onError={(e) => e.target.src = '/images/default.jpg'} />
-        <img src='/images/specificinterior.jpg' className='grid-image' alt='Specific Interior' onError={(e) => e.target.src = '/images/default.jpg'} />
-        <img src='/images/cozydark.jpeg' className='grid-image' alt='Cozy Dark' onError={(e) => e.target.src = '/images/default.jpg'} />
+        <img src='../../images/bedroom.jpeg' className='grid-image' alt='Bedroom' onError={(e) => e.target.src = '/images/default.jpg'} />
+        <img src='../../images/chair.jpg' className='grid-image' alt='Chair' onError={(e) => e.target.src = '/images/default.jpg'} />
+        <img src='../../images/circlemirror.jpg' className='grid-image' alt='Circle Mirror' onError={(e) => e.target.src = '/images/default.jpg'} />
+        <img src='../../images/dinnertable.jpg' className='grid-image' alt='Dinner Table' onError={(e) => e.target.src = '/images/default.jpg'} />
+        <img src='../../images/exteriordesign.jpeg' className='grid-image' alt='Exterior Design' onError={(e) => e.target.src = '/images/default.jpg'} />
+        <img src='../../images/interiorbed.jpg' className='grid-image' alt='Interior Bed' onError={(e) => e.target.src = '/images/default.jpg'} />
+        <img src='../../images/specificinterior.jpg' className='grid-image' alt='Specific Interior' onError={(e) => e.target.src = '/images/default.jpg'} />
+        <img src='../../images/cozydark.jpeg' className='grid-image' alt='Cozy Dark' onError={(e) => e.target.src = '/images/default.jpg'} />
       </div>
 
       {/* Footer and bottom footer components */}
